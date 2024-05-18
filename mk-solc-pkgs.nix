888337fcf897c3615b2bdd0d123d1e294ba744ff
Miao, ZhiCheng <miao@decentral.ee>
2024-05-19 02:21:58 +0300

    nixfmt relevant files

--- mk-solc-pkgs.nix
+++ mk-solc-pkgs.nix
@@ -1,12 +1,17 @@
-{ solc-macos-amd64-list
-} :
+{ solc-macos-amd64-list }:
 
-p : p.lib.foldr (a: b: let
-  pname = "solc_" + (builtins.replaceStrings ["."] ["_"] a.version);
-in b // {
-  "${pname}" = p.callPackage (import ./mk-solc-static.nix) {
-    solc_ver = a.version;
-    solc_sha256 = a.sha256;
-    inherit solc-macos-amd64-list;
-  };
-}) {} (import ./solc-listing.nix)
+p:
+p.lib.foldr (
+  a: b:
+  let
+    pname = "solc_" + (builtins.replaceStrings [ "." ] [ "_" ] a.version);
+  in
+  b
+  // {
+    "${pname}" = p.callPackage (import ./mk-solc-static.nix) {
+      solc_ver = a.version;
+      solc_sha256 = a.sha256;
+      inherit solc-macos-amd64-list;
+    };
+  }
+) { } (import ./solc-listing.nix)
