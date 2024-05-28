ba958487ff0657f4d55562f2dfd7444270b1c64b
Samuel Balco <goodlyrottenapple@gmail.com>
2024-05-28 12:09:52 +0100

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     flake-utils.url = "github:numtide/flake-utils";
     solc-macos-amd64-list-json = {
       # Go to https://github.com/ethereum/solc-bin/blob/gh-pages/macosx-amd64/list.json to obtain a revision
-      url = "file+https://github.com/ethereum/solc-bin/raw/f743ca7/macosx-amd64/list.json";
+      url = "https://github.com/ethereum/solc-bin/raw/f743ca7/macosx-amd64/list.json";
       flake = false;
     };
   };
