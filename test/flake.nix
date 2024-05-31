6c3a139e53e0b069ddf7d1c250f6f177c48c7227
Miao, ZhiCheng <zhicheng.miao@gmail.com>
2024-05-31 23:19:18 +0300

    test example flakes too

--- test/flake.nix
+++ test/flake.nix
@@ -13,22 +13,22 @@
       buildInputs = [
         solc_0_4_26
         solc_0_7_6
-        solc_0_8_25
+        solc_0_8_23
         (solc.mkDefault pkgs solc_0_8_25)
       ];
     };
     devShell.x86_64-darwin = with pkgs; mkShell {
       buildInputs = [
         solc_0_7_6
-        solc_0_8_25
-        (solc.mkDefault pkgs solc_0_8_23)
+        solc_0_8_23
+        (solc.mkDefault pkgs solc_0_8_25)
       ];
     };
     devShell.aarch64-darwin = with pkgs; mkShell {
       buildInputs = [
         solc_0_7_6
-        solc_0_8_25
-        (solc.mkDefault pkgs solc_0_8_23)
+        solc_0_8_23
+        (solc.mkDefault pkgs solc_0_8_25)
       ];
     };
   };
