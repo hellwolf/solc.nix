b4e8dda79ac095c0ec2a16b7bba36e7bc1669f02
Miao, ZhiCheng <zhicheng.miao@gmail.com>
2024-06-02 21:22:59 +0300

    fix ci

--- test/flake.nix
+++ test/flake.nix
@@ -1,41 +1,45 @@
 {
   inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     solc = {
       url = "path:../.";
+      inputs.flake-utils.follows = "flake-utils";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, solc }: let
-    pkgs = import nixpkgs {
-      system = "x86_64-linux";
-      overlays = [
-        solc.overlay
-      ];
-    };
-  in {
-    devShell.x86_64-linux = with pkgs; mkShell {
-      buildInputs = [
-        solc_0_4_26
-        solc_0_7_6
-        solc_0_8_23
-        (solc.mkDefault pkgs solc_0_8_25)
-      ];
-    };
-    devShell.x86_64-darwin = with pkgs; mkShell {
-      buildInputs = [
-        solc_0_7_6
-        solc_0_8_23
-        (solc.mkDefault pkgs solc_0_8_25)
-      ];
-    };
-    devShell.aarch64-darwin = with pkgs; mkShell {
-      buildInputs = [
-        solc_0_7_6
-        solc_0_8_23
-        (solc.mkDefault pkgs solc_0_8_25)
-      ];
-    };
-  };
+  outputs =
+    {
+      self,
+      flake-utils,
+      nixpkgs,
+      solc,
+    }:
+    flake-utils.lib.eachSystem
+      [
+        "x86_64-linux"
+        "x`86_64-darwin"
+        "aarch64-darwin"
+      ]
+      (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ solc.overlay ];
+          };
+        in
+        {
+          devShell.default =
+            with pkgs;
+            mkShell {
+              buildInputs = [
+                solc_0_7_6
+                solc_0_8_23
+                (solc.mkDefault pkgs solc_0_8_25)
+              ];
+            };
+        }
+      );
 }
