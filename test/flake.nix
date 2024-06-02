6571879baaec1c9f2d0e5c2c05be6ab628b7f534
Miao, ZhiCheng <zhicheng.miao@gmail.com>
2024-06-02 21:37:25 +0300

    fix ci

--- test/flake.nix
+++ test/flake.nix
@@ -34,17 +34,23 @@
           devShells.default =
             with pkgs;
             mkShell {
-              buildInputs = [
-                solc_0_8_23
-                (solc.mkDefault pkgs solc_0_8_25)
-              ] ++
-              (if system == "x86_64-linux" then
-                [solc_0_4_26 solc_0_7_6]
-               else if system == "x86_64-darwin" then
-                 [solc_0_7_6]
-               else
-                 []
-              );
+              buildInputs =
+                [ solc_0_8_23 ]
+                ++ (
+                  if system == "x86_64-linux" then
+                    [
+                      solc_0_4_26
+                      solc_0_7_6
+                      (solc.mkDefault pkgs solc_0_8_25)
+                    ]
+                  else if system == "x86_64-darwin" then
+                    [
+                      solc_0_7_6
+                      (solc.mkDefault pkgs solc_0_8_25)
+                    ]
+                  else
+                    [ (solc.mkDefault pkgs solc_0_8_23) ]
+                );
             };
         }
       );
